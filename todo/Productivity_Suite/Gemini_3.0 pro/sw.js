f5b30a882c15737695ba3de6cd7e06be0bb3ab2c
nattzchukwumela <chukwumelapraise@gmail.com>
2025-11-27 08:35:40 +0100

    gemini created it's own todo app

--- todo/Productivity_Suite/Gemini_3.0 pro/sw.js
+++ todo/Productivity_Suite/Gemini_3.0 pro/sw.js
@@ -1,36 +1,10 @@
 const CACHE_NAME = 'prod-suite-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json'
-];
+const ASSETS = ['/', '/index.html'];
 
-// Install Event
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
-    );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Fetch Event
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-// Activate Event (Cleanup old caches)
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.filter((key) => key !== CACHE_NAME)
-                    .map((key) => caches.delete(key))
-            );
-        })
-    );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
